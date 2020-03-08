eb17ebdec91193b44c4e000c3be895df8d78fe76
Jayant Malik <dev.jayantmalik@gmail.com>
2020-03-08 13:38:38 +0530

    Updates

---
+++ precache-manifest.db8c3f87cf661b36e23528513d598923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfdf247da35a82e5d64b369342278b6",
+    "url": "/project-2-finance-redux/index.html"
+  },
+  {
+    "revision": "3fdd60748dc2e5d3c0f7",
+    "url": "/project-2-finance-redux/static/js/2.b857d2ee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project-2-finance-redux/static/js/2.b857d2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084d951af21dd32ad615",
+    "url": "/project-2-finance-redux/static/js/main.64dbe57d.chunk.js"
+  },
+  {
+    "revision": "14ae809004f40b6cdd14",
+    "url": "/project-2-finance-redux/static/js/runtime-main.56d2342f.js"
+  }
+]);
